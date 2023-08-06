d94af392dcb55fabdf2c031a425515287668e10e
HarunAGCA <harun39117@gmail.com>
2023-08-06 18:19:48 +0300

    Try different variable assignment strategies

---
+++ variableAssignment.py
@@ -0,0 +1,19 @@
+# Multiple varible assignment with multiple values
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# Multiple varible assignment with one value
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+# Unpacking a  list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
