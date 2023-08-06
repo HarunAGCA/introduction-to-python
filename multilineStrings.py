09c17c662ae8c320c217577b325fdab5f9b27396
HarunAGCA <harun39117@gmail.com>
2023-08-06 23:24:50 +0300

    Use multiline string notation with single and double quotes

---
+++ multilineStrings.py
@@ -0,0 +1,17 @@
+multiline_with_double_quotes = """
+Hello,
+    This is a multiline
+    string 
+    with double quotes
+"""
+
+print(multiline_with_double_quotes)
+
+multiline_with_single_quotes = '''
+Hello,
+    This is a multiline
+    string 
+    with double quotes
+'''
+
+print(multiline_with_single_quotes)
