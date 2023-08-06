36cef70293d0d305231e9399cf20f95755827170
HarunAGCA <harun39117@gmail.com>
2023-08-07 00:10:48 +0300

    Use in keyword to search a phrase in a text

---
+++ checkString.py
@@ -0,0 +1,9 @@
+text_to_be_searched = "This is a text to be searched"
+
+if("text" in text_to_be_searched):
+    print('The string contains the word "text"')
+
+if "x" not in text_to_be_searched:
+    print('The string contains the word "x"')
+
+
