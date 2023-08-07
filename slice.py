1ee6a3ccf2dbb7f231039144081db04838c830ac
HarunAGCA <harun39117@gmail.com>
2023-08-07 19:39:46 +0300

    Use slice method for substring

---
+++ slice.py
@@ -0,0 +1,17 @@
+# Start index inclusive and end index exclusive
+
+# Between start and end index
+b = "Hello, World!"
+print(b[2:5])
+
+# From beginning to end index
+b = "Hello, World!"
+print(b[:5])
+
+# From start index to end of string
+b = "Hello, World!"
+print(b[2:])
+
+# Use negative indexes to start the slice from the end of the string
+b = "Hello, World!"
+print(b[-5:-2])
