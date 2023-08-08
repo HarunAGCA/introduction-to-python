dcee545b222c57e2f119087de12c5b9758ce9b23
HarunAGCA <harun39117@gmail.com>
2023-08-08 22:33:21 +0300

    Use dictionary collection type

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
