6d1134b358edb4d2681e99c8017dcea934beffde
HarunAGCA <harun39117@gmail.com>
2023-08-08 18:57:58 +0300

    Use logical operators to combine conditional statements

---
+++ logicalOperators.py
@@ -0,0 +1,14 @@
+# Python Logical Operators
+# Logical operators are used to combine conditional statements:
+
+x = 5
+
+print(not(x > 3 and x < 10))
+
+x = 5
+
+print(x > 3 or x < 4)
+
+x = 5
+
+print(x > 3 and x < 10)
