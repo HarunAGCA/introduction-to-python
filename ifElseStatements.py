3d377b009f32970cea73781917549c2cd8311703
HarunAGCA <harun39117@gmail.com>
2023-08-08 23:00:14 +0300

    Use if else statements

---
+++ ifElseStatements.py
@@ -0,0 +1,15 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+
+# One line if else statement:
+a = 330
+b = 2
+print("A") if a > b else print("B")
