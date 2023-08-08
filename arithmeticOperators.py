eae6c55791827f36baa4ac0e6c3e2f45b2ddd410
HarunAGCA <harun39117@gmail.com>
2023-08-08 18:56:09 +0300

    Use arithmetic operators

---
+++ arithmeticOperators.py
@@ -0,0 +1,15 @@
+# Exponentiation operator
+x = 2
+y = 5
+
+print(x ** y) #same as 2*2*2*2*2
+
+
+
+# Floor division operator
+x = 15
+y = 2
+
+print(x // y) # rounds the result down to the nearest whole number
+
+
