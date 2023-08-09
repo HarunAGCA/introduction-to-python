4c899606ad1c19da9ac942b52b67c4bc61819f9b
HarunAGCA <harun39117@gmail.com>
2023-08-09 22:00:48 +0300

    Implement inheritance

---
+++ inheritance.py
@@ -0,0 +1,18 @@
+# Create Person class as a base class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+# Send Person class as a parameter to Student class to define as the base class
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+x.printname()
